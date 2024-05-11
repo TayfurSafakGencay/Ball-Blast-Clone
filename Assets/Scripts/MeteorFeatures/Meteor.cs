37fe6014217f477604a095717fc851108bf114cb
TayfurSafakGencay <safak.gencay@gmail.com>
2024-05-11 19:56:04 +0300

    New: Meteors added.

---
+++ Assets/Scripts/MeteorFeatures/Meteor.cs
@@ -0,0 +1,137 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace MeteorFeatures
+{
+  public class Meteor : MonoBehaviour
+  {
+    private MeteorSpawner _meteorSpawner;
+    
+    private Rigidbody2D _rb2D;
+    
+    [SerializeField]
+    private float _jumpForce;
+
+    [SerializeField]
+    private TextMeshProUGUI _healthText;
+
+    private int _health;
+
+    private int _maxHealth;
+
+    private int _stage;
+
+    private void Awake()
+    {
+      _rb2D = GetComponent<Rigidbody2D>();
+      _meteorSpawner = GameManager.Instance.meteorSpawner;
+    }
+
+    private void Update()
+    {
+      float zRotation = Time.deltaTime * 30;
+      transform.Rotate(0f, 0f, zRotation);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      if (other.gameObject.CompareTag("Wall"))
+      {
+        _rb2D.velocity = new Vector2(0, _rb2D.velocity.y);
+        ApplyForce();
+      }
+      else if (other.gameObject.CompareTag("Ground"))
+      {
+        _rb2D.velocity = new Vector2(_rb2D.velocity.x, _jumpForce);
+      }
+      else if (other.gameObject.CompareTag("Player"))
+      {
+        GameManager.Instance.GameOver();
+      }
+    }
+
+    public void SetMeteorInitialStats(int health, int stage, bool initial = true)
+    {
+      _stage = stage;
+      _health = health;
+      _maxHealth = health;
+      
+      SetLocalScale();
+      SetHealthText();
+      
+      if (initial)
+        ApplyForce(75);
+    }
+
+    public void TakeDamage(int damage)
+    {
+      _health -= damage;
+      SetHealthText();
+      
+      if (_health <= 0)
+        Die();
+    }
+    
+    private void Die()
+    {
+      if (_stage > 1)
+      {
+        _meteorSpawner.DestroyMeteor(_stage, _maxHealth, transform.position);
+      }
+      
+      Destroy(gameObject);
+    }
+
+    private void SetHealthText()
+    {
+      _healthText.text = _health.ToString();
+    }
+
+    public void RebirthForce(string key)
+    {
+      switch (key)
+      {
+        case "left":
+          transform.localPosition -= new Vector3(transform.localScale.x, 0, 0);
+          break;
+        case "right":
+          transform.localPosition += new Vector3(transform.localScale.x, 0, 0);
+          break;
+      }
+      
+      _rb2D.velocity = new Vector2(_rb2D.velocity.x, _jumpForce / 1.5f);
+
+      ApplyForce(40, key);
+    }
+
+    private void ApplyForce(int force = 50, string key = null)
+    {
+      switch (key)
+      {
+        case null when transform.position.x > 0:
+          _rb2D.AddForce(Vector2.left * force);
+          break;
+        case null:
+          _rb2D.AddForce(Vector2.right * force);
+          break;
+        case "left":
+          _rb2D.AddForce(Vector2.left * force);
+          break;
+        case "right":
+          _rb2D.AddForce(Vector2.right * force);
+          break;
+      }
+    }
+
+    private void SetLocalScale()
+    {
+      transform.localScale = _stage switch
+      {
+        3 => new Vector3(0.8f, 0.8f),
+        2 => new Vector3(0.4f, 0.4f),
+        _ => new Vector3(0.2f, 0.2f)
+      };
+    }
+  }
+}
---
+++ Assets/Scripts/MeteorFeatures/MeteorSpawner.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace MeteorFeatures
+{
+  public class MeteorSpawner : MonoBehaviour
+  {
+    [Space(15)]
+    [SerializeField]
+    private List<Transform> _meteorSpawners;
+
+    [Space(15)]
+    [SerializeField]
+    private Transform _meteorContainer;
+
+    [Space(15)]
+    [SerializeField]
+    private GameObject _meteorObject;
+
+    private GameManager _gameManager;
+
+    private float _time;
+
+    private float _remainingTime;
+
+    private void Start()
+    {
+      _gameManager = GameManager.Instance;
+      _remainingTime = 1;
+      
+      SetSpawnersPositions();
+    }
+
+    private void Update()
+    {
+      _remainingTime -= Time.deltaTime;
+
+      if (_remainingTime > 0f) return;
+
+      _time = Random.Range(3, 8);
+      _remainingTime = _time;
+      SpawnMeteor();
+    }
+
+    private void SpawnMeteor()
+    {
+      Transform meteorSpawner = GameManager.GetRandomElementFromList(_meteorSpawners);
+
+      GameObject meteor = Instantiate(_meteorObject, meteorSpawner.transform.position, Quaternion.identity, _meteorContainer);
+      Meteor meteorComponent = meteor.GetComponent<Meteor>();
+      meteorComponent.SetMeteorInitialStats(3, 3);
+    }
+
+    public void DestroyMeteor(int stage, int maxHealth, Vector3 position)
+    {
+      int newStage = stage - 1;
+      int newMaxHealth = maxHealth / 2;
+      if (newMaxHealth <= 0) newMaxHealth = 1;
+      
+      RebirthMeteor(position, newMaxHealth, newStage, "left");
+      RebirthMeteor(position, newMaxHealth, newStage, "right");
+    }
+
+    private void RebirthMeteor(Vector3 position, int newMaxHealth, int newStage, string key)
+    {
+      GameObject meteorObject = Instantiate(_meteorObject, position, Quaternion.identity, _meteorContainer);
+      Meteor meteorComponent = meteorObject.GetComponent<Meteor>();
+      meteorComponent.SetMeteorInitialStats(newMaxHealth, newStage, false);
+      meteorComponent.RebirthForce(key);
+    }
+
+    private Vector2 screenSize;
+
+    private void SetSpawnersPositions()
+    {
+      screenSize.x = Vector2.Distance(_gameManager.Camera.ScreenToWorldPoint(new Vector2(0, 0)), 
+        _gameManager.Camera.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f; 
+      screenSize.y = Vector2.Distance(_gameManager.Camera.ScreenToWorldPoint(new Vector2(0, 0)), 
+        _gameManager.Camera.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
+      
+      _meteorSpawners[0].position = new Vector3(_gameManager.Camera.transform.position.x + screenSize.x + 0.1f, _gameManager.Camera.transform.position.y + 2.5f, 0);
+      _meteorSpawners[1].position = new Vector3(_gameManager.Camera.transform.position.x - screenSize.x - 0.1f, _gameManager.Camera.transform.position.y + 2.5f, 0);
+    }
+  }
+}
