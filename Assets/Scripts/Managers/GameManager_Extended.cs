a5d5ba7b0159d22a9e418dee93fee8fae3044f2a
TayfurSafakGencay <safak.gencay@gmail.com>
2024-05-12 10:04:17 +0300

    New: Now, GameManager is partial class.

---
+++ Assets/Scripts/Managers/GameManager_Extended.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Vo;
+
+namespace Managers
+{
+  public sealed partial class GameManager
+  {
+    public static T GetRandomElementFromList<T>(List<T> list)
+    {
+      Random random = new();
+      int index = random.Next(0, list.Count);
+      return list[index];
+    }
+
+    public static async Task Delay(float second)
+    {
+      await Task.Delay((int)(second * 1000));
+    }
+    
+    // Getter - Setter
+    public bool GetIsGameStarted()
+    {
+      return _isGameStarted;
+    }
+
+    public bool GetIsGameFinished()
+    {
+      return _isGameFinished;
+    }
+
+    public LevelStatsVo GetLevelStats()
+    {
+      return _levelStatsVo;
+    }
+  }
+}
